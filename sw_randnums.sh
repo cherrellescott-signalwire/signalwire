9a8d6ce640d6b5833fded9eedc4a73ffeda1eca6
joshebosh <bitbucket@joshebosh.email>
2019-05-23 11:39:37 -0400

    updating sw_randnums.sh

--- sw_randnums.sh
+++ sw_randnums.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-SW_SPACE="YourSpaceName"       # do not include '.signalwire.com' here, only space name
+SW_SPACE_URL="example.signalwire.com"
 SW_PROJ_KEY="AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE"
 SW_TOKEN="PTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
-sw_bulk_number_available () {
+if [ "${SW_SPACE_URL:-}" == "example.signalwire.com" ] || \
+   [ "${SW_PROJ_KEY:-}" == "AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE" ] || \
+   [ "${SW_TOKEN:-}" == "PTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" ]; then
+    printf "ERROR: Please change the SW_SPACE, SW_PROJ_KEY, and SW_TOKEN variables with your actual info\n"
+    exit
+fi
 
+sw_bulk_number_available () {
     if [ ! ${1:-} ]; then printf "Please specify <local|tollfree> [1-50]\n"; exit; fi
     if [ $1 == local ]; then
 	# excluding VT and NH for now
@@ -15,7 +21,7 @@ sw_bulk_number_available () {
 			   SD TN TX UT VA    WA WV WI WY)
 	RAND=$(( RANDOM % (47 - 0 + 1 ) + 0 ))
 	RANDOM_REGION=${REGION[${RAND:-1}]}
-	PHONE_LIST=$(curl --silent https://$SW_SPACE.signalwire.com/api/laml/2010-04-01/Accounts/$SW_PROJ_KEY/AvailablePhoneNumbers/US/Local.json \
+	PHONE_LIST=$(curl --silent https://$SW_SPACE_URL/api/laml/2010-04-01/Accounts/$SW_PROJ_KEY/AvailablePhoneNumbers/US/Local.json \
 	     -X GET \
 	     -d "InRegion=$RANDOM_REGION" \
 	     -u "$SW_PROJ_KEY:$SW_TOKEN" \
@@ -25,7 +31,7 @@ sw_bulk_number_available () {
 	declare -a AREACODE=( 833 844 855 866 877 888 )
 	RAND=$(( RANDOM % 6 ))
 	RANDOM_AREACODE=${AREACODE[${RAND:-4}]}
-	PHONE_LIST=$(curl --silent https://$SW_SPACE.signalwire.com/api/laml/2010-04-01/Accounts/$SW_PROJ_KEY/AvailablePhoneNumbers/US/TollFree.json \
+	PHONE_LIST=$(curl --silent https://$SW_SPACE_URL/api/laml/2010-04-01/Accounts/$SW_PROJ_KEY/AvailablePhoneNumbers/US/TollFree.json \
 	     -X GET \
 	     -d "AreaCode=$RANDOM_AREACODE" \
 	     -u "$SW_PROJ_KEY:$SW_TOKEN" \
@@ -37,7 +43,6 @@ sw_bulk_number_available () {
 }
 
 sw_bulk_number_purchase () {
-
     if [ ! ${1:-} ]; then 
 	read -p "Will your numbers be \"local\" or \"tollfree\"? [local]: " TYPE
 	TYPE=${TYPE:-local}
@@ -66,6 +71,15 @@ sw_bulk_number_purchase () {
 	ACTION=${4:-dry}
     fi
     
+    if [ $TOTAL -ge 200 ]; then
+	printf "HIGH COST WARNING: For accounts with proper permissions, you can specify up to 5000 numbers with this script."
+    fi
+
+    if [ $TOTAL -gt 5000 ]; then
+	printf "ERROR: Cannot use this script to purchase more than 5000 numbers per project. Please contact sales.\n"
+	exit
+    fi
+
     if [ $TOTAL -lt $ATOTAL ]; then
 	printf "ERROR: Check your math, individual numbers from area code should not be more than total numbers.\n"
 	exit
@@ -79,10 +93,10 @@ sw_bulk_number_purchase () {
 
     if [ $ACTION != dry ]; then
 	printf "\n\n"
-	read -p "ATTENTION: You're purchasing a total of $TOTAL random numbers, for $ATOTAL $TYPE numbers from $(( $TOTAL / $ATOTAL)) random area code. Hit [Enter] to continue, or Ctrl+c to abort..."
+	read -p "ATTENTION: You're purchasing a total of $TOTAL random number(s), for $ATOTAL $TYPE number(s) from $(( $TOTAL / $ATOTAL)) random area code. Hit [Enter] to continue, or Ctrl+c to abort..."
     else
 	printf "\n\n"
-	read -p "You're doing a dry run total of $TOTAL random numbers, for $ATOTAL $TYPE numbers from $(( $TOTAL / $ATOTAL)) random area code, [Enter] to continue, or Ctrl+c to abort..."
+	read -p "You're doing a dry run total of $TOTAL random number(s), for $ATOTAL $TYPE number(s) from $(( $TOTAL / $ATOTAL)) random area code, [Enter] to continue, or Ctrl+c to abort..."
     fi
 
     t=0
@@ -92,7 +106,7 @@ sw_bulk_number_purchase () {
 	while read -r PHONE; do
 	    if  [ $ACTION != dry ]; then
 		printf "$p: puchased $PHONE\n"
-		curl https://$SW_SPACE.signalwire.com/api/laml/2010-04-01/Accounts/$SW_PROJ_KEY/IncomingPhoneNumbers.json \
+		curl https://$SW_SPACE_URL/api/laml/2010-04-01/Accounts/$SW_PROJ_KEY/IncomingPhoneNumbers.json \
 		     -X POST \
 		     --data-urlencode "PhoneNumber=$PHONE" \
 		     -u "$SW_PROJ_KEY:$SW_TOKEN"
@@ -109,11 +123,11 @@ printf "In the future you can execute command as such:\n\n"
 printf "    bash sw_random.sh <local|tollfree> [1-50] [1-200]\n\n"
 printf "Where [1-50] is how many you want per area code, and [1-200] is total numbers.\n\n"
 printf "examples:\n\n"
-printf "    bash sw_random.sh local                 <--- will prompt you for more info\n"
-printf "    bash sw_random.sh tollfree              <--- will prompt you for more info \n"
-printf "    bash sw_random.sh local 2 10            <--- will purchase 10 random local numbers, 2 from 5 random area codes\n"
-printf "    bash sw_random.sh tollfree 5 100        <--- will purchase 10 random tollfree numbers, 5 from 20 random area codes\n"
-printf "    bash sw_random.sh local 8 16 dry        <--- dry run, dont acutally purchase 8 numbers from 2 random area codes, just see what's available\n"
-printf "    bash sw_random.sh local 8 16 purchase   <--- real run, acutally purchase 8 local numbers from 2 random area codes\n\n\n"
+printf "    $0 local                 <--- will prompt you for more info\n"
+printf "    $0 tollfree              <--- will prompt you for more info\n"
+printf "    $0 local 2 10            <--- will purchase 10 random local numbers, 2 from 5 random area codes\n"
+printf "    $0 tollfree 5 100        <--- will purchase 10 random tollfree numbers, 5 from 20 random area codes\n"
+printf "    $0 local 8 16 dry        <--- dry run, dont acutally purchase 8 numbers from 2 random area codes, just see what's available\n"
+printf "    $0 local 8 16 purchase   <--- real run, acutally purchase 8 local numbers from 2 random area codes\n\n\n"
 sw_bulk_number_purchase $@
 exit 0
